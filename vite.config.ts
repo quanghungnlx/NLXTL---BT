97695fb518c611c9eb37bfc3eeb566a8d5e52728
quanghungnlx <quanghungnlx@gmail.com>
2026-07-13 13:49:40 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: ' /NLXTL-BT/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
